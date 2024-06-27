5ec5c52757fb4d96fc7348e19dc92d26b77648fa
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-06-27 09:27:56 +0300

    Add project files.

---
+++ MailSender/Controllers/EmailController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailSender.Models;
+
+namespace MailSender.Controllers
+{
+    public class EmailController : Controller
+    {
+        [HttpGet]
+        public IActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendEmail(EmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("TestName", "Не скажу"));
+
+                var recipients = model.Recipients.Split(';').Select(r => r.Trim()).ToList();
+                foreach (var recipient in recipients)
+                {
+                    message.To.Add(new MailboxAddress(recipient, recipient));
+                }
+
+                message.Subject = model.Subject;
+
+                var bodyBuilder = new BodyBuilder
+                {
+                    HtmlBody = model.Body
+                };
+
+                if (model.Attachments != null && model.Attachments.Count > 0)
+                {
+                    foreach (var attachment in model.Attachments)
+                    {
+                        if (attachment.Length > 0)
+                        {
+                            using (var stream = new MemoryStream())
+                            {
+                                await attachment.CopyToAsync(stream);
+                                bodyBuilder.Attachments.Add(attachment.FileName, stream.ToArray());
+                            }
+                        }
+                    }
+                }
+
+                message.Body = bodyBuilder.ToMessageBody();
+
+                using (var client = new SmtpClient())
+                {
+
+                    await client.ConnectAsync("smtp.gmail.com", 587, false);
+                    await client.AuthenticateAsync("Не скажу", "Не скажу");
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+
+                return RedirectToAction("SendEmail", new { message = "Email отправлен успешно!" });
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ MailSender/Models/EmailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MailSender.Models
+{
+    public class EmailViewModel
+    {
+        [Required]
+        [Display(Name = "Получатели")]
+        public string Recipients { get; set; }
+
+        [Required]
+        [Display(Name = "Тема")]
+        public string Subject { get; set; }
+
+        [Required]
+        [Display(Name = "Сообщение")]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        [Display(Name = "Вложения")]
+        public List<IFormFile> Attachments { get; set; }
+    }
+}
